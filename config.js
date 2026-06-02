ca5ff9073cb642b2fc470247a398c8ba9783f4a6
643698056li-code <643698056li@gmail.com>
2026-06-02 16:46:36 +0800

    Create config.js

---
+++ config.js
@@ -0,0 +1,14 @@
+const SUPABASE_URL =
+'https://bvldfdvnxrrhcesvthwx.supabase.co';
+
+const SUPABASE_KEY =
+'https://bvldfdvnxrrhcesvthwx.supabase.co/rest/v1/';
+
+const BUCKET_NAME =
+'dundun-images';
+
+const supabaseClient =
+supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
