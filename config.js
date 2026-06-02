b474f5d067bf5d447abe5549275390388fc8c65a
643698056li-code <643698056li@gmail.com>
2026-06-02 16:49:16 +0800

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const SUPABASE_URL =
 'https://bvldfdvnxrrhcesvthwx.supabase.co';
 
 const SUPABASE_KEY =
-'https://bvldfdvnxrrhcesvthwx.supabase.co/rest/v1/';
+'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ2bGRmZHZueHJyaGNlc3Z0aHd4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODAzODMwNjYsImV4cCI6MjA5NTk1OTA2Nn0.4cKf_iIC0O4n5-9OjdRY8E2ocNAZ9bqqWzSa1gCEJ58';
 
 const BUCKET_NAME =
 'dundun-images';
