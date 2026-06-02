7dabfedb1d184e37e54023df4bbbb5b73a9a6cc5
643698056li-code <643698056li@gmail.com>
2026-06-02 16:53:38 +0800

    Create app.js

---
+++ js/app.js
@@ -0,0 +1,88 @@
+async function loadPhotos(){
+
+```
+const { data , error } =
+await supabaseClient
+.from('photos')
+.select('*')
+.order('id',{ascending:false});
+
+if(error){
+    console.log(error);
+    return;
+}
+
+const gallery =
+document.getElementById('gallery');
+
+gallery.innerHTML='';
+
+data.forEach(photo=>{
+
+    gallery.innerHTML += `
+    <div class="card">
+
+        <img src="${photo.image_url}">
+
+        <h3>
+            ${photo.title || ''}
+        </h3>
+
+    </div>
+    `;
+});
+```
+
+}
+
+async function loadTimeline(){
+
+```
+const { data , error } =
+await supabaseClient
+.from('timeline')
+.select('*')
+.order(
+    'event_date',
+    {ascending:false}
+);
+
+if(error){
+    console.log(error);
+    return;
+}
+
+const timeline =
+document.getElementById('timeline');
+
+timeline.innerHTML='';
+
+data.forEach(item=>{
+
+    timeline.innerHTML += `
+
+    <div class="timeline-item">
+
+        <h3>
+            ${item.title}
+        </h3>
+
+        <p>
+            ${item.event_date}
+        </p>
+
+        <p>
+            ${item.content || ''}
+        </p>
+
+    </div>
+
+    `;
+});
+```
+
+}
+
+loadPhotos();
+
+loadTimeline();
